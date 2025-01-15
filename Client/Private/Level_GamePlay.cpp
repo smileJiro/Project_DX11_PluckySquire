6fd5f89e9e77f25960e599aa1a58cf4534c5c6c8
JYBbear <wnsduf_123@naver.com>
2025-01-15 23:00:20 +0900

    commit

--- Client/Private/Level_GamePlay.cpp
+++ Client/Private/Level_GamePlay.cpp
@@ -181,7 +181,7 @@ HRESULT CLevel_GamePlay::Ready_Layer_Monster(const _wstring& _strLayerTag, CGame
 	CBeetle::MONSTER_DESC Monster_Desc;
 	Monster_Desc.iCurLevelID = LEVEL_GAMEPLAY;
 
-	Monster_Desc.tTransform3DDesc.vPosition = _float3(0.0f, 1.0f, 0.0f);
+	Monster_Desc.tTransform3DDesc.vPosition = _float3(10.0f, 1.0f, 10.0f);
 	Monster_Desc.tTransform3DDesc.vScaling = _float3(1.f, 1.f, 1.f);
 
 	if (FAILED(m_pGameInstance->Add_GameObject_ToLayer(LEVEL_GAMEPLAY, TEXT("Prototype_GameObject_Beetle"), LEVEL_GAMEPLAY, _strLayerTag, &pGameObject, &Monster_Desc)))
