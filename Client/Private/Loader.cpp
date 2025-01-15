4606ece7c2148c5219690993895422a4354cfcff
JYBbear <wnsduf_123@naver.com>
2025-01-15 21:09:31 +0900

    .

--- Client/Private/Loader.cpp
+++ Client/Private/Loader.cpp
@@ -214,7 +214,7 @@ HRESULT CLoader::Loading_Level_GamePlay()
     XMMATRIX matPretransform = XMMatrixScaling(1 / 150.0f, 1 / 150.0f, 1 / 150.0f);
 
     if (FAILED(Load_Dirctory_Models_Recursive(LEVEL_GAMEPLAY,
-        TEXT("../Bin/Resources/Models/"), matPretransform)))
+        TEXT("../Bin/Resources/TestModels/"), matPretransform)))
         return E_FAIL;
 
     lstrcpy(m_szLoadingText, TEXT("객체원형(을)를 로딩중입니다."));
