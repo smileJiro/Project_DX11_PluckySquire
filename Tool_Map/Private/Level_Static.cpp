e85d9a818109a36217bc91548270db2d93397e75
seulck <krrkik@gmail.com>
2025-01-26 20:00:25 +0900

    진짜 파일정리

--- Tool_Map/Private/Level_Static.cpp
+++ Tool_Map/Private/Level_Static.cpp
@@ -2,6 +2,7 @@
 #include "Level_Static.h"
 #include "Level_Loading.h"
 #include "GameInstance.h"
+#include "BackGround.h"
 
 CLevel_Static::CLevel_Static(ID3D11Device* _pDevice, ID3D11DeviceContext* _pContext)
 	: CLevel(_pDevice, _pContext)
@@ -10,22 +11,32 @@ CLevel_Static::CLevel_Static(ID3D11Device* _pDevice, ID3D11DeviceContext* _pCont
 
 HRESULT CLevel_Static::Initialize()
 {
+	CBackGround::BACKGROUND_DESC Desc = {};
+
+	Desc.strTextureComponentTag = L"Prototype_Component_Texture_MapTool_Logo";
+	CGameObject* pGameObject = nullptr;
+	if (FAILED(m_pGameInstance->Add_GameObject_ToLayer(LEVEL_STATIC, TEXT("Prototype_GameObject_BackGround"),
+		LEVEL_STATIC, L"Layer_Background", &pGameObject, &Desc)))
+		return E_FAIL;
+
+
+
  	return S_OK;
 }
 
 void CLevel_Static::Update(_float _fTimeDelta)
 {
 	if (KEY_DOWN(KEY::F1))
-	Event_LevelChange(LEVEL_LOADING, LEVEL_TOOL_3D_MAP);
+		Event_LevelChange(LEVEL_LOADING, LEVEL_TOOL_3D_MAP);
 	else if (KEY_DOWN(KEY::F2))
-	Event_LevelChange(LEVEL_LOADING, LEVEL_TOOL_2D_MAP);
+		Event_LevelChange(LEVEL_LOADING, LEVEL_TOOL_2D_MAP);
 	
 }
 
 HRESULT CLevel_Static::Render()
 {
 #ifdef _DEBUG
-	SetWindowText(g_hWnd, TEXT("F1-> 3D Tool F1-> 2D Tool"));
+	SetWindowText(g_hWnd, TEXT("F1-> 3D Tool F2-> 2D Tool"));
 #endif
 	return S_OK;
 }
--- Tool_Map/Public/Task_Manager.h
+++ Tool_Map/Public/Task_Manager.h
@@ -56,26 +56,46 @@ private :
 
 public :
 	HRESULT					Initialize(CImguiLogger* _pLogger);
+
+	// 진행상황을 체크하고, 진행이 완료되었을 때 스레드 정지. 이후 큐에 작업이 쌓여있는지 확인 후, 작업이 남아있으면 다시금 스레드를 작동시킨다. 
 	HRESULT					Update();
+
+
 public :
-	HRESULT					Open_ParsingDialog(const wstring& _strLayerName);
+	
+	// 3D Map .umap(.json) 파일을 툴에 파싱해오는 작업을 등록하는 메소드
 	void					Register_Parsing(const string& _strParsingFileName, const wstring& _strLayerName, _bool _isClear = true);
-	HRESULT					Parsing();
+	// 3D Map .umap(.json) 파일을 선택하는 다이얼로그를 호출함 
+	HRESULT					Open_ParsingDialog(const wstring& _strLayerName);
+	
 
+	// 3D 모델 리패킹 작업을 queue에 등록하는 메소드
 	void					Register_RePackaging(const wstring& _strParsingFileName, CMapObject* pMapObject);
+	// 리패킹 진행메소드. 
 	HRESULT					RePackaging();
+	// 파싱 진행메소드
+	HRESULT					Parsing();
+private:
+	// 해당 코드작업 진행시 json 라이브러리에 대한 이해가 부족하여 실제 탐색은 해당 재귀메소드로 진행했다. Parsing() 내부에서만 쓰임. 아쉬움 
+	HRESULT					Parsing(json _jsonObj);
 
-
+public :
+	// 3D Map SaveFile Result File(모델 사용이력 불러올때 쓰인다. 같은 경로에 저장된다.)
 	HRESULT					Export_SaveResult_ToJson(const _wstring _strFIlePath, const vector<_string>& _SaveModelProtoNames, _bool isStatic = false);
 
+	// 로그 저장용 ㄱ-
+	HRESULT					Export_Result(const _wstring& _strFilePath, const _wstring& _strFileName, vector<_string> _ExportStrings );
 
+
+#pragma region Get_Set
 
 	const vector<_string>&	Get_EgnoreObjectNames() const { return m_EgnoreObjectNames; };
 	HRESULT					Push_EgnoreModelName(_string _strModelName) 
 	{ if (!m_isLoading) m_EgnoreObjectNames.push_back(_strModelName); }
+
 	_bool					Is_Loading() { return m_isLoading; }
 	_bool					Is_Loading_Complete() { return m_isLoadComp; }
-
+#pragma endregion
 
 private:
 	//void					Model_ReParsing(const _wstring _strModelPath);
@@ -85,7 +105,6 @@ private:
 private :
 	void					Open_Parsing();
 	void					Open_RePackaging();
-	HRESULT					Parsing(json _jsonObj);
 	HRESULT					Find_Override_Material_Texture(CMapObject* _pMapObject, json _jsonObj);
 	HRESULT					Find_Override_Material_Color(CMapObject* _pMapObject, json _jsonObj);
 
@@ -104,11 +123,12 @@ private :
 	HANDLE										m_hThread = {};
 
 	vector<_string>								m_MapObjectNames;
+	// 파싱할 때 무시할 모델 이름
 	vector<_string>								m_EgnoreObjectNames;
 	vector<_string>								m_RePackaging;
 	vector<pair<_wstring,CMapObject*>>			m_RepackNeededModels;
 
-	_wstring									m_strUmapJsonPath = L"..\\Bin\\json\\";
+	_wstring									m_strUmapJsonPath = L"..\\Bin\\json\\3DMapJson\\";
 
 	TASK										m_tCurWork;
 
