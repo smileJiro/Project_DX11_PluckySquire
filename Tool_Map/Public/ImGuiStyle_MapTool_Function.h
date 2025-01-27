7620ac1fab42098886bcb055596a188c5ff4b0e9
seul <krrkik@gmail.com>
2025-01-27 18:09:33 +0900

    2d 모델툴 수정작업중

--- Tool_Map/Public/ImGuiStyle_MapTool_Function.h
+++ Tool_Map/Public/ImGuiStyle_MapTool_Function.h
@@ -17,5 +17,7 @@ namespace Map_Tool
 	_bool ActiveButton(IMGUI_MAPTOOL_BUTTON_STYLE_TYPE _eButtonType, _bool _isActivePlag, const string& _strTrueText, const string& _strFalseText);
 	void  Begin_Draw_ColorButton(const string& key, ImVec4 vStartColor);
 	void  End_Draw_ColorButton();
+	_bool InputText(const char* _label, std::string& _str, ImGuiInputTextFlags _flags = 0);
+
 
 }
