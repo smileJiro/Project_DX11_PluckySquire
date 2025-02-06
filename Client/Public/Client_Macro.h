950819a1bf5ab8ceef8b3c6ec9da88c60e617c9c
smileJiro <xodnd1478@gmail.com>
2025-02-06 13:50:07 +0900

    book_2d 렌더타겟 삭제

--- Client/Public/Client_Macro.h
+++ Client/Public/Client_Macro.h
@@ -4,11 +4,6 @@
 namespace Client
 {
 #define NUMVIEWPORTS 1
-#define FRICTION_COEFFICIENT 0.3f
-#define SKILLBALLUI_MAX 8
-#define SKILLBALLPOS_X 	1177.f
-#define SKILLBALLPOS_Y 	465.f
-#define SKILLBALLGAP_X 	87.f
 #define PX_SUPPORT_PVD 1 /* pvd 를 위한 플래그 */
 
 
