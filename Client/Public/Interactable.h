353b03cbb262b6a63c163925a295f031bd11af86
jiwan <akrhwkwk@gmail.com>
2025-02-07 20:03:38 +0900

    IInteractable 클래스 주석 추가

--- Client/Public/Interactable.h
+++ Client/Public/Interactable.h
@@ -6,8 +6,13 @@ class CPlayer;
 class IInteractable 
 {
 public:
+	//상호작용 성공했을 때 호출됨.
+	//상호작용 시 동작할 내용을 구현
 	virtual void Interact(CPlayer* _pUser)abstract;
+	//상호작용 가능한 상태인지 반환하는 함수.
+	//자신이 상호작용 불가능한 상태면 false 반환
 	virtual _bool Is_Interactable(CPlayer* _pUser)abstract;
+	//사용자와의 거리를 반환하는 함수. 
 	virtual _float Get_Distance(CPlayer* _pUser)abstract;
 };
 
