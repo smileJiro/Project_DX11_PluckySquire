bd82798d7805832da0d8fb7ae5bafc12de8387a2
jiwan <akrhwkwk@gmail.com>
2025-02-15 18:48:19 +0900

    OBJECT_GROUP 주석 추가

--- Client/Public/Client_Enum.h
+++ Client/Public/Client_Enum.h
@@ -86,13 +86,13 @@ namespace Client
 	enum OBJECT_GROUP // 아래 GROUP은 예시임. 편한대로 사용하시면 됨.
 	{
 		NONE =						0x00,
-		PLAYER =					0x01,//0
-		PLAYER_TRIGGER =			0x02,//1
+		PLAYER =					0x01,//0  (플레이어 몸통)
+		PLAYER_TRIGGER =			0x02,//1  (플레이어의 상호작용 트리거, 주변 물체 감지 트리거)
 		MONSTER =					0x04,//2
 		MAPOBJECT =					0x08, //3
-		PLAYER_PROJECTILE =			0x10,//4
+		PLAYER_PROJECTILE =			0x10,//4  (플레이어의 공격체)
 		MONSTER_PROJECTILE =		0x20,//5
-		TRIGGER_OBJECT =		    0x40,//6
+		TRIGGER_OBJECT =		    0x40,//6  (카메라 트리거, 전구 등에서 씀)
 		RAY_OBJECT =				0x80,//7
 		INTERACTION_OBEJCT =		0x100,//8 (상호작용 하려면 GameObject의 Group으로 설정해야 함)
 		BLOCKER = 					0x200,	//9	(현재 2D 플랫포머에서 중력 컴포넌트랑 연계중. 3D에서 플레이어 몸통이랑 블락중)
