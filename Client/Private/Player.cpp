e867b6e66ed406545a7f9ff4538bcf263b0069d9
jiwan <akrhwkwk@gmail.com>
2025-02-19 12:58:19 +0900

    ii

--- Client/Private/Player.cpp
+++ Client/Private/Player.cpp
@@ -821,14 +821,15 @@ void CPlayer::On_Collision2D_Stay(CCollider* _pMyCollider, CCollider* _pOtherCol
             if (Check_ReplaceInteractObject(pInteractable))
             {
                 m_pInteractableObject = pInteractable;
-                STATE eStat = m_pStateMachine->Get_CurrentState()->Get_StateID();
+       /*         STATE eStat = m_pStateMachine->Get_CurrentState()->Get_StateID();
                 if(STATE::START_PORTAL != eStat
                     && STATE::JUMPTO_PORTAL!= eStat
                     && STATE::EXIT_PORTAL != eStat)
                 {
 					cout << "Sate : " << eStat << endl;
-                    m_pPortal = dynamic_cast<CPortal*>(m_pInteractableObject);
-                }
+              
+                }*/
+                m_pPortal = dynamic_cast<CPortal*>(m_pInteractableObject);
             }
         }
         
