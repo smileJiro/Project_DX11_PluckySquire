6d78d254d4f88b135175f1915867ec9cf20b7dad
leea0a <leea0a@naver.com>
2025-02-28 17:02:17 +0900

    UI - 다이얼로그에 따라 애니메이션 변경, E 진행중, 6챕터 NPC 및 다이얼로그 연결

--- Client/Private/Level_Chapter_02.cpp
+++ Client/Private/Level_Chapter_02.cpp
@@ -55,6 +55,7 @@
 #include "ShopItemBG.h"
 #include "MapObjectFactory.h"
 #include "Interaction_E.h"
+#include "NPC_Manager.h"
 
 #include "NPC.h"
 #include "Loader.h"
@@ -1150,7 +1151,7 @@ HRESULT CLevel_Chapter_02::Ready_Layer_Item(const _wstring& _strLayerTag)
 HRESULT CLevel_Chapter_02::Ready_Layer_NPC(const _wstring& _strLayerTag)
 {
 	CNPC::NPC_DESC NPCDesc;
-
+	CGameObject* pGameObject = { nullptr };
 	NPCDesc.iCurLevelID = m_eLevelID;
 	NPCDesc.tTransform2DDesc.vInitialPosition = _float3(0.f, 0.f, 0.f);
 	NPCDesc.tTransform3DDesc.vInitialScaling = _float3(1.f, 1.f, 1.f);
@@ -1158,8 +1159,10 @@ HRESULT CLevel_Chapter_02::Ready_Layer_NPC(const _wstring& _strLayerTag)
 	NPCDesc.iMainIndex = 0;
 	NPCDesc.iSubIndex = 0;
 	//wsprintf(NPCDesc.strDialogueIndex, TEXT("DJ_Moobeard_Dialogue_01"));
-	if (FAILED(m_pGameInstance->Add_GameObject_ToLayer(m_eLevelID, TEXT("Prototype_GameObject_NPC_OnlySocial"), NPCDesc.iCurLevelID, _strLayerTag, &NPCDesc)))
+	if (FAILED(m_pGameInstance->Add_GameObject_ToLayer(m_eLevelID, TEXT("Prototype_GameObject_NPC_OnlySocial"), NPCDesc.iCurLevelID, _strLayerTag, &pGameObject, &NPCDesc)))
 		return E_FAIL;
+
+	CNPC_Manager::GetInstance()->Set_OnlyNpc(static_cast<CNPC_OnlySocial*>(pGameObject));
 
 
 	wsprintf(NPCDesc.strDialogueIndex, L"");
---
+++ Client/Private/NPC_Manager.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include "NPC_Manager.h"
+
+IMPLEMENT_SINGLETON(CNPC_Manager)
+
+CNPC_Manager::CNPC_Manager()
+	:m_pGameInstance(CGameInstance::GetInstance())
+{
+	Safe_AddRef(m_pGameInstance);
+}
+
+
+HRESULT CNPC_Manager::Level_Exit(_int iCurLevelID, _int _iChangeLevelID, _int _iNextChangeLevelID)
+{
+
+	if (nullptr != m_pNPC)
+	{
+		Safe_Release(m_pNPC);
+		m_pNPC = nullptr;
+	}
+
+	if (nullptr != m_pOnlyNpc)
+	{
+		Safe_Release(m_pOnlyNpc);
+		m_pOnlyNpc = nullptr;
+	}
+
+
+	return S_OK;
+}
+
+HRESULT CNPC_Manager::Level_Logo_Exit(_int _iChangeLevelID, _int _iNextChangeLevelID)
+{
+
+	return S_OK;
+}
+
+HRESULT CNPC_Manager::Level_Enter(_int _iChangeLevelID)
+{
+	if (nullptr == m_pOnlyNpc)
+		return S_OK;
+
+	m_pOnlyNpc->NextLevelLoadJson(_iChangeLevelID);
+
+	return S_OK;
+}
+
+
+void CNPC_Manager::Free()
+{
+	Safe_Release(m_pOnlyNpc);
+	Safe_Release(m_pNPC);
+
+	__super::Free();
+}
+
