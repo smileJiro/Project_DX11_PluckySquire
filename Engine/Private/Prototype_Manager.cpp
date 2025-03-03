9a39a4ddbaa38b83cdc709bbcb206e1495916dd5
jiwan <akrhwkwk@gmail.com>
2025-03-03 16:05:00 +0900

    테스트 코디ㅡ 삭제

--- Engine/Private/Prototype_Manager.cpp
+++ Engine/Private/Prototype_Manager.cpp
@@ -121,10 +121,6 @@ void CPrototype_Manager::Free()
     {
         for (auto& Pair : m_pPrototypes[i])
         {
-
-            if (Pair.first == TEXT("Prototype_GameObject_CubeMap")) {
-                int a = 0;
-            }
             Safe_Release(Pair.second);
 
         }
