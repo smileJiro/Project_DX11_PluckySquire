1d15ce24d4d19df778cee48d910e1d2c51357fe0
nyongking <niyongking@gmail.com>
2025-03-03 21:34:16 +0900

    push의 신.

--- Client/Public/UI.h
+++ Client/Public/UI.h
@@ -132,7 +132,7 @@ public:
 	virtual void Free() override;
 
 protected:
-	CShader*		m_pShaderComs[COORDINATE_LAST] = {};
+	CShader*		m_pShaderComs[COORDINATE_LAST] = {nullptr, nullptr};
 	CTexture*		m_pTextureCom = {nullptr};
 	CVIBuffer_Rect* m_pVIBufferCom = { nullptr };
 	C2DModel*		m_pModelCom = { nullptr };
