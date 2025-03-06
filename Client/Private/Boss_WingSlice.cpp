4622aea320c4f7e584552b45cf840cede9d7b75f
JYBbear <wnsduf_123@naver.com>
2025-03-06 17:55:38 +0900

    boss purple

--- Client/Private/Boss_WingSlice.cpp
+++ Client/Private/Boss_WingSlice.cpp
@@ -1,29 +1,27 @@
 #include "stdafx.h"
-#include "Boss_HomingBall.h"
+#include "Boss_WingSlice.h"
 #include "ModelObject.h"
 #include "Pooling_Manager.h"
 #include "GameInstance.h"
 
-CBoss_HomingBall::CBoss_HomingBall(ID3D11Device* _pDevice, ID3D11DeviceContext* _pContext)
+CBoss_WingSlice::CBoss_WingSlice(ID3D11Device* _pDevice, ID3D11DeviceContext* _pContext)
 	: CProjectile_Monster(_pDevice, _pContext)
 {
 }
 
-CBoss_HomingBall::CBoss_HomingBall(const CBoss_HomingBall& _Prototype)
-	: CProjectile_Monster(_Prototype)
+CBoss_WingSlice::CBoss_WingSlice(const CBoss_WingSlice& _Prototype)
+    : CProjectile_Monster(_Prototype)
 {
 }
 
-HRESULT CBoss_HomingBall::Initialize_Prototype()
+HRESULT CBoss_WingSlice::Initialize_Prototype()
 {
 	return S_OK;
 }
 
-HRESULT CBoss_HomingBall::Initialize(void* _pArg)
+HRESULT CBoss_WingSlice::Initialize(void* _pArg)
 {
     PROJECTILE_MONSTER_DESC* pDesc = static_cast<PROJECTILE_MONSTER_DESC*>(_pArg);
-
-    m_fOriginSpeed = pDesc->tTransform3DDesc.fSpeedPerSec;
 
     if (FAILED(Ready_ActorDesc(pDesc)))
         return E_FAIL;
@@ -45,50 +43,50 @@ HRESULT CBoss_HomingBall::Initialize(void* _pArg)
     }
     Safe_Delete(pDesc->pActorDesc);
 
-
-    m_fMinDistance = 10.f;
-
 	return S_OK;
 }
 
-void CBoss_HomingBall::Priority_Update(_float _fTimeDelta)
+void CBoss_WingSlice::Priority_Update(_float _fTimeDelta)
 {
     __super::Priority_Update(_fTimeDelta);
 }
 
-void CBoss_HomingBall::Update(_float _fTimeDelta)
-{
-    Homing(_fTimeDelta);
-
-     __super::Update(_fTimeDelta);
-}
-
-void CBoss_HomingBall::Late_Update(_float _fTimeDelta)
+void CBoss_WingSlice::Update(_float _fTimeDelta)
 {
 
-	__super::Late_Update(_fTimeDelta);
+    /*_vector vDir = m_pTarget->Get_FinalPosition() - Get_FinalPosition();
+    m_pControllerTransform->Go_Direction(vDir, _fTimeDelta);*/
+    m_pControllerTransform->Go_Straight(_fTimeDelta);
+
+    __super::Update(_fTimeDelta);
 }
 
-HRESULT CBoss_HomingBall::Render()
+void CBoss_WingSlice::Late_Update(_float _fTimeDelta)
+{
+
+  	__super::Late_Update(_fTimeDelta);
+}
+
+HRESULT CBoss_WingSlice::Render()
 {
     __super::Render();
     return S_OK;
 }
 
-void CBoss_HomingBall::OnTrigger_Enter(const COLL_INFO& _My, const COLL_INFO& _Other)
+void CBoss_WingSlice::OnTrigger_Enter(const COLL_INFO& _My, const COLL_INFO& _Other)
 {
     __super::OnTrigger_Enter(_My, _Other);
 }
 
-void CBoss_HomingBall::OnTrigger_Stay(const COLL_INFO& _My, const COLL_INFO& _Other)
+void CBoss_WingSlice::OnTrigger_Stay(const COLL_INFO& _My, const COLL_INFO& _Other)
 {
 }
 
-void CBoss_HomingBall::OnTrigger_Exit(const COLL_INFO& _My, const COLL_INFO& _Other)
+void CBoss_WingSlice::OnTrigger_Exit(const COLL_INFO& _My, const COLL_INFO& _Other)
 {
 }
 
-HRESULT CBoss_HomingBall::Cleanup_DeadReferences()
+HRESULT CBoss_WingSlice::Cleanup_DeadReferences()
 {
     if (FAILED(__super::Cleanup_DeadReferences()))
         return E_FAIL;
@@ -96,7 +94,7 @@ HRESULT CBoss_HomingBall::Cleanup_DeadReferences()
     if (nullptr == m_pTarget)
     {
 #ifdef _DEBUG
-        cout << "HOMINGBALL_Cleanup : NO PLAYER" << endl;
+        cout << "WingSlice_Cleanup : NO PLAYER" << endl;
 #endif // _DEBUG
         return S_OK;
     }
@@ -110,69 +108,17 @@ HRESULT CBoss_HomingBall::Cleanup_DeadReferences()
     return S_OK;
 }
 
-void CBoss_HomingBall::Active_OnEnable()
+void CBoss_WingSlice::Active_OnEnable()
 {
     __super::Active_OnEnable();
 }
 
-void CBoss_HomingBall::Active_OnDisable()
+void CBoss_WingSlice::Active_OnDisable()
 {
-    m_pControllerTransform->Set_SpeedPerSec(m_fOriginSpeed);
-    m_isHoming = false;
-    XMStoreFloat3(&m_vDir, XMVectorZero());
-
     __super::Active_OnDisable();
 }
 
-void CBoss_HomingBall::Homing(_float _fTimeDelta)
-{
-    _vector vDir = m_pTarget->Get_FinalPosition() - Get_FinalPosition();
-
-    //if (false == m_isHoming)
-    //{
-    //    if (2.f <= m_fAccTime)
-    //    {
-    //        m_isHoming = true;
-    //        m_pControllerTransform->Set_SpeedPerSec(m_fOriginSpeed * 3.f);
-    //    }
-    //    m_pControllerTransform->Go_Straight(_fTimeDelta);
-    //}
-    //else
-    //{
-    // 
-    //    m_pControllerTransform->Set_AutoRotationYDirection(vDir);
-    //    m_pControllerTransform->Update_AutoRotation(_fTimeDelta);
-    //    m_pControllerTransform->Go_Direction(vDir, _fTimeDelta);
-    //}
-
-    if (false == m_isHoming)
-    {
-        _vector vLook = Get_ControllerTransform()->Get_State(CTransform::STATE_LOOK);
-        if (1.f <= m_fAccTime)
-        {
-            m_isHoming = true;
-            m_pControllerTransform->Set_SpeedPerSec(m_fOriginSpeed * 4.f);
-            XMStoreFloat3(&m_vDir, XMVector3Normalize(vDir));
-        }
-    }
-    else
-    {
-        _float fAngle = XMVectorGetX(XMVector3Dot(XMVector3Normalize(vDir), XMLoadFloat3(&m_vDir)));
-        //거리가 가깝지 않고 뒤에 있지 않을 때 방향전환
-        if (m_fMinDistance < XMVectorGetX(XMVector3Length(vDir)) && 0.f < XMVectorGetX(XMVector3Dot(XMVector3Normalize(vDir), XMLoadFloat3(&m_vDir))))
-        {
-            XMStoreFloat3(&m_vDir, XMVector3Normalize(vDir));
-        }
-
-        _vector vLook = Get_ControllerTransform()->Get_State(CTransform::STATE_LOOK);
-		_float fScaleZ = Get_ControllerTransform()->Get_Scale().z;
-		Get_ControllerTransform()->Set_State(CTransform::STATE_LOOK, XMVector3Normalize(XMVectorLerp(XMVector3Normalize(vLook), XMLoadFloat3(&m_vDir), 0.2f)) * fScaleZ);
-    }
-
-    m_pControllerTransform->Go_Straight(_fTimeDelta);
-}
-
-HRESULT CBoss_HomingBall::Ready_ActorDesc(void* _pArg)
+HRESULT CBoss_WingSlice::Ready_ActorDesc(void* _pArg)
 {
     PROJECTILE_MONSTER_DESC* pDesc = static_cast<PROJECTILE_MONSTER_DESC*>(_pArg);
 
@@ -183,9 +129,9 @@ HRESULT CBoss_HomingBall::Ready_ActorDesc(void* _pArg)
     ActorDesc->pOwner = this;
 
     /* Actor의 회전축을 고정하는 파라미터 */
-    ActorDesc->FreezeRotation_XYZ[0] = false;
+    ActorDesc->FreezeRotation_XYZ[0] = true;
     ActorDesc->FreezeRotation_XYZ[1] = false;
-    ActorDesc->FreezeRotation_XYZ[2] = false;
+    ActorDesc->FreezeRotation_XYZ[2] = true;
 
     /* Actor의 이동축을 고정하는 파라미터 (이걸 고정하면 중력도 영향을 받지 않음. 아예 해당 축으로의 이동을 제한하는)*/
     ActorDesc->FreezePosition_XYZ[0] = false;
@@ -195,7 +141,7 @@ HRESULT CBoss_HomingBall::Ready_ActorDesc(void* _pArg)
     /* 사용하려는 Shape의 형태를 정의 */
     SHAPE_CAPSULE_DESC* ShapeDesc = new SHAPE_CAPSULE_DESC;
     ShapeDesc->fRadius = 2.f;
-    ShapeDesc->fHalfHeight = 2.f;
+    ShapeDesc->fHalfHeight = 20.f;
 
     /* 해당 Shape의 Flag에 대한 Data 정의 */
     SHAPE_DATA* ShapeData = new SHAPE_DATA;
@@ -204,8 +150,7 @@ HRESULT CBoss_HomingBall::Ready_ActorDesc(void* _pArg)
     ShapeData->eMaterial = ACTOR_MATERIAL::DEFAULT; // PxMaterial(정지마찰계수, 동적마찰계수, 반발계수), >> 사전에 정의해둔 Material이 아닌 Custom Material을 사용하고자한다면, Custom 선택 후 CustomMaterial에 값을 채울 것.
     ShapeData->isTrigger = true;                    // Trigger 알림을 받기위한 용도라면 true
     ShapeData->iShapeUse = (_uint)SHAPE_USE::SHAPE_BODY;
-    //XMStoreFloat4x4(&ShapeData->LocalOffsetMatrix, XMMatrixRotationZ(XMConvertToRadians(90.f)) * XMMatrixTranslation(ShapeDesc->fHalfHeight, ShapeDesc->fRadius + ShapeDesc->fHalfHeight, 0.0f)); // Shape의 LocalOffset을 행렬정보로 저장.
-	XMStoreFloat4x4(&ShapeData->LocalOffsetMatrix, XMMatrixRotationZ(XMConvertToRadians(90.f)) * XMMatrixTranslation(0.f, 0.f, 0.f)); // Shape의 LocalOffset을 행렬정보로 저장.
+    XMStoreFloat4x4(&ShapeData->LocalOffsetMatrix, XMMatrixTranslation(0.0f, 0.f, 0.0f)); // Shape의 LocalOffset을 행렬정보로 저장.
 
     /* 최종으로 결정 된 ShapeData를 PushBack */
     ActorDesc->ShapeDatas.push_back(*ShapeData);
@@ -223,12 +168,12 @@ HRESULT CBoss_HomingBall::Ready_ActorDesc(void* _pArg)
     return S_OK;
 }
 
-HRESULT CBoss_HomingBall::Ready_Components()
+HRESULT CBoss_WingSlice::Ready_Components()
 {
     return S_OK;
 }
 
-HRESULT CBoss_HomingBall::Ready_PartObjects()
+HRESULT CBoss_WingSlice::Ready_PartObjects()
 {
     CModelObject::MODELOBJECT_DESC BodyDesc{};
 
@@ -236,10 +181,8 @@ HRESULT CBoss_HomingBall::Ready_PartObjects()
     BodyDesc.iCurLevelID = m_iCurLevelID;
     BodyDesc.isCoordChangeEnable = m_pControllerTransform->Is_CoordChangeEnable();
 
-
-    //테스트용, 원 게임에서는 구체인데 일단 불릿 모델 써봄
     BodyDesc.strShaderPrototypeTag_3D = TEXT("Prototype_Component_Shader_VtxMesh");
-    BodyDesc.strModelPrototypeTag_3D = TEXT("S_FX_CMN_Sphere_01");
+    BodyDesc.strModelPrototypeTag_3D = TEXT("S_FX_CMN_Sonic_02");
     BodyDesc.iModelPrototypeLevelID_3D = m_iCurLevelID;
     BodyDesc.iShaderPass_3D = (_uint)PASS_VTXMESH::DEFAULT;
 
@@ -249,7 +192,7 @@ HRESULT CBoss_HomingBall::Ready_PartObjects()
     BodyDesc.pParentMatrices[COORDINATE_3D] = m_pControllerTransform->Get_WorldMatrix_Ptr(COORDINATE_3D);
 
     BodyDesc.tTransform3DDesc.vInitialPosition = _float3(0.0f, 0.0f, 0.0f);
-    BodyDesc.tTransform3DDesc.vInitialScaling = _float3(1.f, 1.f, 1.f);
+    BodyDesc.tTransform3DDesc.vInitialScaling = _float3(20.f, 20.f, 20.f);
     BodyDesc.tTransform3DDesc.fRotationPerSec = XMConvertToRadians(90.f);
     BodyDesc.tTransform3DDesc.fSpeedPerSec = 10.f;
 
@@ -258,38 +201,41 @@ HRESULT CBoss_HomingBall::Ready_PartObjects()
         return E_FAIL;
 
     static_cast<C3DModel*>(static_cast<CModelObject*>(m_PartObjects[PART_BODY])->Get_Model(COORDINATE_3D))->Set_MaterialConstBuffer_UseAlbedoMap(0, false, true);
-    static_cast<C3DModel*>(static_cast<CModelObject*>(m_PartObjects[PART_BODY])->Get_Model(COORDINATE_3D))->Set_MaterialConstBuffer_Albedo(0, _float4(1.f, 0.f, 0.f, 1.f), true);
+    static_cast<C3DModel*>(static_cast<CModelObject*>(m_PartObjects[PART_BODY])->Get_Model(COORDINATE_3D))->Set_MaterialConstBuffer_Albedo(0, _float4(1.f, 1.f, 1.f, 1.f), true);
+
+    //m_PartObjects[PART_BODY]->Get_ControllerTransform()->RotationXYZ(_float3(45.f, 0.f, -45.f));
+    m_PartObjects[PART_BODY]->Get_ControllerTransform()->RotationQuaternion(_float3(XMConvertToRadians(90.f), XMConvertToRadians(-90.f),XMConvertToRadians( 0.f)));
 
     return S_OK;
 }
 
-CBoss_HomingBall* CBoss_HomingBall::Create(ID3D11Device* _pDevice, ID3D11DeviceContext* _pContext)
+CBoss_WingSlice* CBoss_WingSlice::Create(ID3D11Device* _pDevice, ID3D11DeviceContext* _pContext)
 {
-    CBoss_HomingBall* pInstance = new CBoss_HomingBall(_pDevice, _pContext);
+    CBoss_WingSlice* pInstance = new CBoss_WingSlice(_pDevice, _pContext);
 
     if (FAILED(pInstance->Initialize_Prototype()))
     {
-        MSG_BOX("Failed to Created : CBoss_HomingBall");
+        MSG_BOX("Failed to Created : CBoss_WingSlice");
         Safe_Release(pInstance);
     }
 
     return pInstance;
 }
 
-CGameObject* CBoss_HomingBall::Clone(void* _pArg)
+CGameObject* CBoss_WingSlice::Clone(void* _pArg)
 {
-    CBoss_HomingBall* pInstance = new CBoss_HomingBall(*this);
+    CBoss_WingSlice* pInstance = new CBoss_WingSlice(*this);
 
     if (FAILED(pInstance->Initialize(_pArg)))
     {
-        MSG_BOX("Failed to Cloned : CBoss_HomingBall");
+        MSG_BOX("Failed to Cloned : CBoss_WingSlice");
         Safe_Release(pInstance);
     }
 
     return pInstance;
 }
 
-void CBoss_HomingBall::Free()
+void CBoss_WingSlice::Free()
 {
     __super::Free();
 }
