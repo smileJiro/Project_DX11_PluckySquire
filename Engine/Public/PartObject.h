78b5480d9d21df1deb936676e3980517ae8628b2
seul <krrkik@gmail.com>
2025-03-10 19:28:14 +0900

    ^^

--- Engine/Public/PartObject.h
+++ Engine/Public/PartObject.h
@@ -33,7 +33,7 @@ public:
 	_float3					Get_FinalScale() const override; // PartObject는 최종 매트릭스에서 위치값 추출해서 리턴.
 	virtual _float3			Get_FinalScale(COORDINATE _eCoord) const override;
 	const _float4x4*		Get_FinalWorldMatrix_Ptr(COORDINATE _eCoord) { return &m_WorldMatrices[_eCoord]; }
-	virtual _matrix		Get_FinalWorldMatrix() override;
+	virtual _matrix			Get_FinalWorldMatrix() override;
 	void Set_SocketMatrix(COORDINATE _eCoord, const _float4x4* _pSocketMatrix) { m_pSocketMatrix[_eCoord] = _pSocketMatrix; }
 	void Set_ParentMatrix(COORDINATE _eCoord, const _float4x4* _pSocketMatrix) { m_pParentMatrices[_eCoord] = _pSocketMatrix; }
 
