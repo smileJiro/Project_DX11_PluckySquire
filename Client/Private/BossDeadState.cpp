abede6b42cfc46425024395525eb297bfcd8bd37
nyongking <niyongking@gmail.com>
2025-03-18 16:22:36 +0900

    for pull

--- Client/Private/BossDeadState.cpp
+++ Client/Private/BossDeadState.cpp
@@ -5,6 +5,8 @@
 #include "Monster.h"
 #include "FSM.h"
 
+#include "Effect_Manager.h"
+
 CBossDeadState::CBossDeadState()
 {
 }
@@ -31,6 +33,51 @@ void CBossDeadState::State_Update(_float _fTimeDelta)
 		return;
 	if (nullptr == m_pOwner)
 		return;
+
+	m_fAccTime += _fTimeDelta;
+
+	_matrix matWorld = m_pOwner->Get_WorldMatrix();
+
+	switch (m_iEffectCount)
+	{
+	case 0:
+		if (0.f <= m_fAccTime)
+		{
+			CEffect_Manager::GetInstance()->Active_EffectMatrix(TEXT("BossDead1"), true, matWorld);
+			++m_iEffectCount;
+		}
+		break;
+	case 1:
+		if (0.45f <= m_fAccTime)
+		{
+			matWorld *= XMMatrixRotationAxis(XMVectorSet(0.f, 1.f, 0.f, 0.f), 90.f);
+			matWorld.r[3] += XMVectorSet(0.f, 0.f, -3.5f, 0.f);
+			CEffect_Manager::GetInstance()->Active_EffectMatrix(TEXT("BossDead1"), true, matWorld);
+			++m_iEffectCount;
+		}
+		break;
+	case 2:
+		if (0.9f <= m_fAccTime)
+		{
+			matWorld *= XMMatrixRotationAxis(XMVectorSet(0.f, 1.f, 0.f, 0.f), 160.f);
+			matWorld.r[3] += XMVectorSet(1.5f, 3.f, -1.f, 0.f);
+			CEffect_Manager::GetInstance()->Active_EffectMatrix(TEXT("BossDead1"), true, matWorld);
+			++m_iEffectCount;
+		}
+	case 3:
+	{
+		if (1.35f <= m_fAccTime)
+		{
+			matWorld *= XMMatrixRotationAxis(XMVectorSet(0.f, 1.f, 0.f, 0.f), 225.f);
+			matWorld.r[3] += XMVectorSet(0.f, 2.f, -4.f, 0.f);
+			CEffect_Manager::GetInstance()->Active_EffectMatrix(TEXT("BossDead1"), true, matWorld);
+			++m_iEffectCount;
+		}
+	}
+	default:
+		break;
+	}
+
 
 	//if (true == m_pOwner->Get_AnimChangeable())
 	//	Event_ChangeBossState(BOSS_STATE::IDLE, m_pFSM);
