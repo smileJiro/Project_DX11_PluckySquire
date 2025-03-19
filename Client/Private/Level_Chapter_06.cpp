5942d042f825b6433980d04ceb7babdb76a976f9
jiwan <akrhwkwk@gmail.com>
2025-03-20 00:46:54 +0900

    6챕터 디버그 코드 삭제

--- Client/Private/Level_Chapter_06.cpp
+++ Client/Private/Level_Chapter_06.cpp
@@ -253,18 +253,6 @@ HRESULT CLevel_Chapter_06::Initialize(LEVEL_ID _eLevelID)
 //
 //#pragma endregion
 
-				/* 4. 파트 바디 생성 */
-	CFatherPart_Prop::FATHERPART_PROP_DESC Desc{};
-	Desc.iCurLevelID = LEVEL_CHAPTER_6;
-	Desc.iFatherPartID = CFatherGame::FATHER_BODY;
-	Desc.Build_2D_Transform(_float2(-0.0, -0.0), _float2(200.0f, 200.0f));
-	CGameObject* pGameObject = nullptr;
-	if (FAILED(m_pGameInstance->Add_GameObject_ToLayer(LEVEL_CHAPTER_6, TEXT("Prototype_GameObject_FatherPart_Prop"), LEVEL_CHAPTER_6, TEXT("Layer_FatherPart_Prop"), &pGameObject, &Desc)))
-		assert(nullptr);
-
-	CSection_Manager::GetInstance()->Add_GameObject_ToCurSectionLayer(pGameObject, SECTION_2D_PLAYMAP_TRIGGER);
-
-
 
 	/* Collision Check Matrix */
 	// 그룹필터 추가 >> 중복해서 넣어도 돼 내부적으로 걸러줌 알아서 
@@ -356,7 +344,7 @@ void CLevel_Chapter_06::Update(_float _fTimeDelta)
 {
 	if (KEY_DOWN(KEY::K))
 	{
-		CFatherPart_Prop::FATHERPART_PROP_DESC Desc{};
+		/*CFatherPart_Prop::FATHERPART_PROP_DESC Desc{};
 		Desc.iCurLevelID = LEVEL_CHAPTER_6;
 		Desc.iFatherPartID = CFatherGame::FATHER_BODY;
 		Desc.Build_2D_Transform(_float2(-0.0, -0.0), _float2(200.0f, 200.0f));
@@ -365,7 +353,7 @@ void CLevel_Chapter_06::Update(_float _fTimeDelta)
 			assert(nullptr);
 
 		CSection_Manager::GetInstance()->Add_GameObject_ToCurSectionLayer(pGameObject, SECTION_2D_PLAYMAP_TRIGGER);
-
+*/
 	}
 	CFatherGame::GetInstance()->Update();
 	CExcavatorGame::GetInstance()->Update(_fTimeDelta);
