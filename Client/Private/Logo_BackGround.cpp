60e2b20b7004d93790fef15f59889b591a6a2657
leea0a <leea0a@naver.com>
2025-03-21 09:45:17 +0900

    사운드 - 8장 입장 진행 중

--- Client/Private/Logo_BackGround.cpp
+++ Client/Private/Logo_BackGround.cpp
@@ -55,8 +55,9 @@ HRESULT CLogo_BackGround::Initialize(void* _pArg)
 
 	case MAIN_HUMGRUMP :
 	{
-		m_pGameInstance->Start_BGM(_wstring(L"LCD_MUS_C09_MAGNIFICENTHUMGRUMP_TITLESCREEN_FULL"), 60.f);
+		
 		m_pGameInstance->Start_SFX_Delay(TEXT("A_sfx_C9_OnceUponATime"), 0.15f, 30.f, false);
+		m_pGameInstance->Start_BGM(_wstring(L"LCD_MUS_C09_MAGNIFICENTHUMGRUMP_TITLESCREEN_FULL"), 60.f);
 	}
 	break;
 	}
